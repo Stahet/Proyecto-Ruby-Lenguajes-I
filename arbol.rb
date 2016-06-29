f755960f5af52a6d8f720e628dae3054f3932966
Jonnathan Ng <ngjonnathan@gmail.com>
2016-06-28 22:08:17 -0400

    convertir a espacios

--- arbol.rb
+++ arbol.rb
@@ -1,43 +1,43 @@
 module BFS
-	def bfs
-		cola = []
-		cola.unshift(self)
-		while not cola.empty?
-			nodo = cola.pop
-			yield nodo
-			nodo.each do |i|
-				cola.unshift(i)
-			end
-		end
-	end
+    def bfs
+        cola = []
+        cola.unshift(self)
+        while not cola.empty?
+            nodo = cola.pop
+            yield nodo
+            nodo.each do |i|
+                cola.unshift(i)
+            end
+        end
+    end
 
-	def recoger(pred)
-		nodos = []
-		bfs {|x| nodos.push(x) if pred.call(x) }
-		return nodos
-	end
+    def recoger(pred)
+        nodos = []
+        bfs {|x| nodos.push(x) if pred.call(x) }
+        return nodos
+    end
 end
 
 class ArbolBinario
-	include BFS
-	attr_accessor :valor
-	def initialize(valor, izq=nil, der=nil)
-		@valor = valor
-		@izq, @der = izq, der
-	end
+    include BFS
+    attr_accessor :valor
+    def initialize(valor, izq=nil, der=nil)
+        @valor = valor
+        @izq, @der = izq, der
+    end
 
-	def each
-		if @izq != nil
-			yield @izq
-		end
-		if @der != nil
-			yield @der
-		end
-	end
+    def each
+        if @izq != nil
+            yield @izq
+        end
+        if @der != nil
+            yield @der
+        end
+    end
 
-	def to_s
-		"Nodo: #{@valor}"
-	end
+    def to_s
+        "Nodo: #{@valor}"
+    end
 
 end
 
